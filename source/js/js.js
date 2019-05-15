bcc436e1ad714ef0ff6d805ff07b9612d0748f39
AlexyShu <Za9i@bk.ru>
2019-05-15 23:02:56 +0300

    подключила JS

---
+++ source/js/js.js
@@ -0,0 +1,15 @@
+var btn_off = document.querySelector(".page-header__taggle-off");
+var btn_on = document.querySelector(".page-header__taggle-on");
+var popup = document.querySelector(".main-navigation");
+
+btn_off.addEventListener("click", function(evt) {
+  popup.classList.add("main-navigation--closed");
+  btn_off.classList.add("page-header__taggle-off--hidden");
+  btn_on.classList.add("page-header__taggle-on--show");
+});
+
+btn_on.addEventListener("click", function(evt) {
+  popup.classList.remove("main-navigation--closed");
+  btn_off.classList.remove("page-header__taggle-off--hidden");
+  btn_on.classList.remove("page-header__taggle-on--show");
+});
