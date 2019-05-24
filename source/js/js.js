435f47c56ecf1776ff427e717de881624cb4c90b
AlexyShu <Za9i@bk.ru>
2019-05-25 02:12:42 +0300

    защита. Попытка 1

--- source/js/js.js
+++ source/js/js.js
@@ -1,6 +1,6 @@
 var btn_off = document.querySelector(".page-header__taggle-off");
 var btn_on = document.querySelector(".page-header__taggle-on");
-var popup = document.querySelector(".main-navigation");
+var popup = document.querySelector(".page-header");
 
 btn_off.addEventListener("click", function(evt) {
   popup.classList.add("main-navigation--closed");
