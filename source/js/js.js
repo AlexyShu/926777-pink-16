f39f7f348434c530cd7a487d038fb94d727fb826
AlexyShu <Za9i@bk.ru>
2019-05-25 17:03:07 +0300

    переписала js

--- source/js/js.js
+++ source/js/js.js
@@ -2,14 +2,14 @@ var btn_off = document.querySelector(".page-header__taggle-off");
 var btn_on = document.querySelector(".page-header__taggle-on");
 var popup = document.querySelector(".page-header");
 
-btn_off.addEventListener("click", function(evt) {
-  popup.classList.add("menu-closed");
-  btn_off.classList.add("page-header__taggle-off--hidden");
-  btn_on.classList.add("page-header__taggle-on--show");
+btn_on.addEventListener("click", function(evt) {
+  popup.classList.remove("menu-active");
+  btn_on.classList.add("page-header__taggle-on--hidden");
+  btn_off.classList.add("page-header__taggle-off--show");
 });
 
-btn_on.addEventListener("click", function(evt) {
-  popup.classList.remove("menu-closed");
-  btn_off.classList.remove("page-header__taggle-off--hidden");
-  btn_on.classList.remove("page-header__taggle-on--show");
+btn_off.addEventListener("click", function(evt) {
+  popup.classList.remove("menu-active");
+  btn_off.classList.remove("page-header__taggle-off--show");
+  btn_on.classList.remove("page-header__taggle-on--hidden");
 });
